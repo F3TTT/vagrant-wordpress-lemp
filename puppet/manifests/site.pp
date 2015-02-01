b6b2a75d06ef5cb867f970157f0cfe03a1e2ac41
F3TTT <ddoughty@epiqsystems.com>
2015-02-01 16:52:01 -0600

    added forked repo

---
+++ puppet/manifests/site.pp
@@ -0,0 +1,10 @@
+exec {'apt-update':
+  path    => '/usr/bin',
+  command => 'apt-get update'
+}
+
+class {'mysql::install': }
+class {'nginx': }
+class {'php5-fpm': }
+class {'wordpress': }
+class {'phpmyadmin': }
---
+++ puppet/modules/wordpress/manifests/init.pp
@@ -0,0 +1,48 @@
+class wordpress {
+  $mysql_user = 'wordpress'
+  $mysql_pass = 'wordpress'
+  $schema     = 'wordpress'
+
+  $wp_root    = '/vagrant'
+  $wp_dl      = 'http://wordpress.org/latest.tar.gz'
+
+  include setup_db
+  include dl_and_install
+}
+
+class wordpress::setup_db {
+  exec {'create-database':
+    path    => '/usr/bin',
+    unless  => 'mysql -uroot -pvagrant wordpress',
+    command => 'mysql -uroot -pvagrant --execute \'create schema wordpress\''
+  }
+
+  exec {'create-user':
+    path    => '/usr/bin',
+    unless  => "mysql -u${mysql_user} -p${mysql_pass}",
+    command => "mysql -uroot -pvagrant --execute 'grant all on ${schema}.* to ${mysql_user}@localhost identified by \"${mysql_pass}\"'",
+    require => Exec['create-database']
+  }
+}
+
+class wordpress::dl_and_install {
+  exec {'download-wordpress':
+    path    => '/bin:/usr/bin',
+    cwd     => "${wp_root}",
+    unless  => 'test -e latest.tar.gz',
+    command => "wget ${wp_dl}",
+    creates => "${wp_root}/latest.tar.gz"
+  }
+
+  exec {'untar-wordpress':
+    path    => '/bin:/usr/bin',
+    cwd     => "${wp_root}",
+    unless  => 'test -e wp-load.php',
+    command => 'tar xvzf latest.tar.gz; mv wordpress/* .; rmdir wordpress',
+    require => Exec['download-wordpress']
+  }
+
+  file {"${wp_root}/wp-config.php":
+    content => template('wordpress/wp-config.erb')
+  }
+}
